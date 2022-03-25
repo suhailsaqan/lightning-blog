6115fd7c18495e426e14d83463bffff2562328fa
Suhail Saqan <suhailsaqan@users.noreply.github.com>
2022-03-24 20:11:36 -0700

    Update env.ts

--- src/helpers/env.ts
+++ src/helpers/env.ts
@@ -1,8 +1,10 @@
 export const env = {
-  max: Number(process.env['MAX']) || 1000000,
-  min: Number(process.env['MIN']) || 0,
-  lightning_memo: process.env.lightning_memo || "Replit Lightning Invoice",
-  name: process.env['NAME'] || "Replit Lightning",
+  max: Number(process.env["MAX"]) || 1000000,
+  min: Number(process.env["MIN"]) || 0,
+  lightning_memo: process.env.lightning_memo || "Lightning Invoice",
+  name: process.env["NAME"] || "Lightning",
 };
 
-export const lightning_memo = JSON.stringify([["text/plain", env.lightning_memo]]);
+export const lightning_memo = JSON.stringify([
+  ["text/plain", env.lightning_memo],
+]);
