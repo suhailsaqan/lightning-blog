e7ab1468d8b531787bb008b1af923674de304f03
Suhail Saqan <suhailsaqan@users.noreply.github.com>
2022-03-30 12:22:06 -0700

    clean up

--- src/pages/draft.tsx
+++ src/pages/draft.tsx
@@ -2,14 +2,6 @@ import dynamic from "next/dynamic";
 
 import TextEditor from "../components/draftEditor/draftEditor";
 
-// const TextEditor = dynamic(() => import("../components/draftEditor"), {
-//   ssr: false,
-// });
-
 export default function Draft() {
-  return (
-    // <Layout>
-    <TextEditor />
-    // </Layout>
-  );
+  return <TextEditor />;
 }
