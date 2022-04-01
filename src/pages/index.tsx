e7ac1dedd303f08fcd073943ab2578b8a190a1af
Suhail Saqan <suhailsaqan@users.noreply.github.com>
2022-04-01 01:49:38 -0700

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,13 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Suhail's Lightning Paywalled Blog
+            <span className="fancy">.</span>
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <span className="handle">@suhailsaqan</span>
+          <h2>
+            Self-host your own blog with a lightning paywall - no censorship!
+          </h2>
           <SocialList />
         </div>
       </div>
