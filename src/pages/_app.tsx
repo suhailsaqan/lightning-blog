75f8ebdacb56b2d8dc18405de5580a7694466e0a
Suhail Saqan <suhailsaqan@users.noreply.github.com>
2022-04-10 03:04:46 -0700

    fixing build error on vercel

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,12 +8,14 @@ const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
+    // <QueryClientProvider client={queryClient}>
+    <>
       <Head>
         <link rel="shortcut icon" href="/favicon.ico" />
         <title>{process.env["WEBPAGENAME"] || "Lightning Blog"}</title>
       </Head>
       <Component {...pageProps} />
-    </QueryClientProvider>
+      {/* </QueryClientProvider> */}
+    </>
   );
 }
