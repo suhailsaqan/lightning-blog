6b885742edaef7f256755dfca86d18e3878ea22d
Suhail Saqan <suhailsaqan@users.noreply.github.com>
2022-04-10 03:12:39 -0700

    that didnt work, welp

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,14 +8,12 @@ const queryClient = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    // <QueryClientProvider client={queryClient}>
-    <>
+    <QueryClientProvider client={queryClient}>
       <Head>
         <link rel="shortcut icon" href="/favicon.ico" />
         <title>{process.env["WEBPAGENAME"] || "Lightning Blog"}</title>
       </Head>
       <Component {...pageProps} />
-      {/* </QueryClientProvider> */}
-    </>
+    </QueryClientProvider>
   );
 }
